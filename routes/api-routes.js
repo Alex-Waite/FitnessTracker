52c6b1adb6ce3876228fc87f94e79da97f47974a
Alex Waite <alexanderconorwaite@gmail.com>
2020-06-24 03:48:41 +0930

    fleshed out server.js and started setting up api routes

---
+++ routes/api-routes.js
@@ -0,0 +1,23 @@
+const express = require("express")
+const router = require("reouter")
+const Workout = require("../models/workout")
+
+// c
+router.post("/api/?????", function (req, res) {
+
+})
+
+// r
+router.get("/api/?????", function (req, res) {
+
+})
+
+// u   Maybe not needed
+router.get("/api/?????", function (req, res) {
+
+})
+
+// d   Maybe not needed
+router.get("/api/?????", function (req, res) {
+
+})
---
+++ server.js
@@ -0,0 +1,27 @@
+const app = express()
+const express = require('express')
+const path = require('path')
+const mongoose = require('mongoose')
+
+const PORT = process.env.PORT || 4500;
+
+app.use('/', express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+
+
+app.get("/", function (req, res) {
+    res.sendFile("/public/index.html")
+})
+
+app.get("/exercise", function (req, res) {
+    res.sendFile("/public/exercise.html")
+})
+
+app.get("/stats", function (req, res) {
+    res.sendFile("/public/stats.html")
+})
+
+app.use(require("./routes/api-routes"))
+app.listen(PORT, function () {
+    console.log(`APP HOSTED ON PORT ${PORT}, ------> http://localhost:${PORT} <--------`);
+})
