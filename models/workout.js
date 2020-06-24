9faa8e94006dbbbc243274ef5df45b5f6052686e
Alex Waite <alexanderconorwaite@gmail.com>
2020-06-24 22:12:54 +0930

    end wednessday progress, i dont like mongo

--- models/workout.js
+++ models/workout.js
@@ -24,7 +24,7 @@ const workoutSchema = new Schema({
             type: Number,
             required: "What weight are you working with?"
         },
-        time: {
+        duration: {
             type: Number,
             required: "How long did you go for?"
         }
--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,19 +1,34 @@
 const express = require("express")
-const router = require("reouter")
-const Workout = require("../models/workout")
+const router = require("router")
+const db = require("../models")
 
 // c
-router.post("/api/?????", function (req, res) {
-
+router.post("/api/workouts", function (req, res) {
+    db.Workout.create({
+        type: req.body.type,
+        name: req.body.name,
+        duration: req.body.duration,
+        weight: req.body.weight,
+        reps: req.body.reps,
+        sets: req.body.sets,
+        day: req.body.day
+    }).then(function (result) {
+        res.send(result)
+    })
 })
 
 // r
-router.get("/api/?????", function (req, res) {
-
+router.get("/api/workouts", function (req, res) {
+    db.Workout.find({}).sort({
+            day: 1
+        }) // 1 = ascending, -1 = ascending for some reason
+        .then(function (result) {
+            res.json(result)
+        })
 })
 
 // u   Maybe not needed
-router.get("/api/?????", function (req, res) {
+router.update("/api/?????", function (req, res) {
 
 })
 
